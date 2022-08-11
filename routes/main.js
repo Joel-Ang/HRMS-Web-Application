304085cd3f43e4dbd641512a2d0fef0656fa9e7e
Jaky-Dev <joelangkaiyu@gmail.com>
2022-08-11 20:11:57 +0800

    Added view all claims page, claims to navigation bar for all pages, get/post in main.js

--- routes/main.js
+++ routes/main.js
@@ -31,9 +31,10 @@ module.exports = function(app) {
                 if (result.length != 0) {
                     //Create session
                     session = req.session;
-                    //Add username and roleid 
+                    //Add username, staffid and roleid 
                     session.username = req.body.E_id;
                     session.roleid = result[0].Role_id;
+                    session.staffid = result[0].Staff_id;
                     res.redirect("/");
                 }
                 else {
@@ -141,5 +142,99 @@ module.exports = function(app) {
                 res.send("Record: " + req.body.dept + " " + req.body.role + " " + req.body.name + " " + req.body.email + " " + req.body.address + " " + req.body.username + " " + req.body.password);
             }
         });
+    });
+
+    //GET Claims Page
+    app.get("/allClaims", function (req, res) {
+        if (req.session.username){
+            let sqlquery = "SELECT Claim.claim_id, DATE_FORMAT(Claim.date_submitted, '%D-%M-%Y') AS date_submitted, DATE_FORMAT(Claim.date_of_claim, '%D-%M-%Y') AS date_of_claim, Claim_Type.type, Claim.claim_amount, Claim.status" +
+                " FROM Claim" +
+                " JOIN Staff ON Claim.Staff_id = Staff.Staff_id" +
+                " JOIN Claim_Type ON Claim.CT_id = Claim_Type.CT_id" +
+                " WHERE Staff.Staff_id = ?";
+            let staffid = [req.session.staffid];
+            db.query(sqlquery, staffid, (err, result) => {
+            if (err)
+            {
+                    console.log(err);
+                     res.redirect("/");
+            }
+                else
+                    res.render("allClaims.html", { availableClaims: result });
+            });
+        }
+        else
+            res.redirect("/login");
+    });
+
+    app.post("/allClaims", function (req, res) {
+        let buttonValue = req.body.claimButtons;
+        if (buttonValue == 'New Claim') {
+            let sqlquery = "SELECT Staff.Staff_id, Staff.Staff_name, Department.Department_name " +
+                " From Staff" +
+                " Join Department ON Staff.Dept_id = Department.Dept_id";
+            // + " WHERE Staff_id = ?";
+            db.query(sqlquery, (err, staffDetail) => {
+                if (err) res.redirect("/");
+                else {
+                    let sqlclaimquery = "Select * from Claim_type";
+                    db.query(sqlclaimquery, (err, claimType) => {
+                        if (err) res.redirect("/");
+                        else {
+                            console.log(claimType[0]);
+                            res.render('newClaim.html', { staffDetails: staffDetail, claimTypes: claimType });
+                        }
+                    });
+
+                }
+            });
+        }
+        else if (buttonValue == 'Edit Claim') {
+            //res.render('editClaim.html');
+        }
+        else if (buttonValue == 'Delete Claim') {
+
+        }
+        else {
+            res.render("allClaims.html");
+        }
+
+    });
+
+    app.post("/newClaims", function (req, res) {
+        const dateOfClaim = req.body.date + " 00:00:00";
+
+        let date_ob = new Date();
+        let date = date_ob.getDate();
+        let month = date_ob.getMonth() + 1;
+        let year = date_ob.getFullYear();
+        let fullDate = year + "-" + month + "-" + date + " 00:00:00";
+        // prints date & time in YYYY-MM-DD format
+        console.log(fullDate);
+        let values;
+        console.log(req.body.newClaimButtons);
+        let sqlquery = "INSERT INTO Claim (CT_id, Staff_id, date_submitted, claim_amount, date_of_claim, status) values (?,?,?,?,?,?)";
+
+        if (req.body.newClaimButtons == "Submit Claim") {
+            values = [req.body.claimType, req.body.staffID, fullDate, req.body.claimAmount, dateOfClaim, "Pending Approval"];
+        }
+        if (req.body.newClaimButtons == "Save as Draft") {
+            values = [req.body.claimType, req.body.staffID, fullDate, req.body.claimAmount, dateOfClaim, "Saved As Draft"];
+        }
+        if (req.body.newClaimButtons == "Cancel"){
+            res.redirect("/allClaims");
+            return;
+        }
+        console.log(values);
+        db.query(sqlquery, values, (err, result) => {
+            if (err) {
+                res.redirect("/");
+                console.log(err);
+            }
+            else {
+                res.redirect("/allClaims");
+            }
+        });
+        
     });
 }
